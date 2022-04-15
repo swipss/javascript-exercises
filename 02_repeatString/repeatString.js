e27117c33f023e572cb3364d2e9b0adafecdeb8e
Sten V <vassiljevsten1@gmail.com>
2022-04-15 18:03:58 +0300

    Repeat string n amount of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0){
+        return "ERROR"
+    }
+    let output = ''
+    while (num > 0) {
+        output += str
+        num--
+    }
+    return output
 };
 
 // Do not edit below this line
