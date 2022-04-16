06d9f7b513749dd05e5ecf2a0edefa0a65feb1df
Sten V <vassiljevsten1@gmail.com>
2022-04-16 14:20:21 +0300

    Remove element from array with optional args

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    // let args = Array.prototype.slice.call(arguments, 1);
+    
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < args.length; j++) {
+            if (arr[i] === args[j]) {
+                arr.splice(i, 1)                
+            }
+        }
+    }
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < args.length; j++) {
+            if (arr[i] === args[j]) {
+                arr.splice(i, 1)                
+            }
+        }
+    }
+    return arr
+    
+    
 };
 
 // Do not edit below this line
